bd036205eb332c051a6af610ab9f91cef5a617bb
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-09-17 14:07:47 +0530

    ReverseNumber

---
+++ src/com/bridgelabz/ReverseNumber.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to reverse it : ");
+        int num = sc.nextInt();
+        int rev = 0; int rem;
+        while (num!=0){
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        System.out.print("The reversed number is : " + rev);
+    }
+}
