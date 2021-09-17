2bb8a3bdeec6acd5456ad1cff975180c2133a540
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-09-17 13:59:25 +0530

    PrimeNumber

---
+++ src/com/bridgelabz/PrimeNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the last number till you want to check the Prime Number : ");
+        int endNum = sc.nextInt();
+        for (int num = 1; num <= endNum; num++) {
+            int temp = 0;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    temp += 1;
+                }
+            }
+            if (temp == 0) {
+                System.out.println(num);
+            }
+        }
+
+    }
+}
